53500bb647bb5b27814278899e4ffd7e84e9044b
Sabardi <Sabardibahari@gmail.com>
2024-09-30 14:01:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="min-h-screen bg-slate-100 grid place-items-center p-6">
+    <!-- wrapper -->
+    <section v-if="products" class="w-12/4 grid grid-cols-5 gap-3">
+      <template v-for="product in products " :key="product.id">
+        <CardProduk class="col-span-1" :product="product"/>
+      </template>
+    </section>
+
+    <!-- loading state -->
+    <p v-else>
+      loading...
+    </p>
+  </main>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import CardProduk from './components/CardProduk.vue';
+
+const products = ref([]);
+
+onMounted(async () => {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const data = await response.json();
+  products.value = data;
+});
+</script>
---
+++ src/components/CardProduk.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="bg-white">
+        <a :href="product.id">
+            <!-- image -->
+            <img :src="product.image" class="w-full h-1/2">
+            <!-- footer -->
+            <section class="p-6 uppercase text-center">
+                <h1 class="text-xl font-bold">{{ product.title }}</h1>
+            </section>
+        </a>
+    </section>
+</template>
+
+<script setup>
+const props = defineProps({
+    product: Object
+});
+</script>
